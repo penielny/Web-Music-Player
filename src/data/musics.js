5d705732848969aa6403f424dab30d1a9e91100c
penielny <penielnyinaku@gmail.com>
2025-05-06 09:55:01 +0000

    product setup

---
+++ src/data/musics.js
@@ -0,0 +1,471 @@
+export const MUSIC_LIST = [
+
+    {
+        cover_image: "https://i.scdn.co/image/ab67616d0000b273861bef35a79b9856949f830a",
+        title: "Man In The Mirror",
+        release_date: new Date("2025-04-01"),
+        length: "4:49",
+        album: undefined,
+        artist: [
+            {
+                name: "Yaw Craft",
+                image: "https://i.scdn.co/image/ab6761610000101ff910ceb013e7f17069ffdb64",
+            },
+        ],
+    },
+    {
+        cover_image: "https://i.scdn.co/image/ab67616d0000b273d8f0a7c4e9f0e1e5d8f0a7c4",
+        title: "Soma Obi",
+        release_date: new Date("2025-03-15"),
+        length: "3:30",
+        album: undefined,
+        artist: [
+            {
+                name: "Black Sherif",
+                image: "https://i.scdn.co/image/ab6761610000101f3f0a7c4e9f0e1e5d8f0a7c4e",
+            },
+        ],
+    },
+    {
+        cover_image: "https://i.scdn.co/image/ab67616d0000b273a1b2c3d4e5f6a7b8c9d0e1f2",
+        title: "Excellent",
+        release_date: new Date("2025-02-20"),
+        length: "3:45",
+        album: undefined,
+        artist: [
+            {
+                name: "KOJO BLAK",
+                image: "https://i.scdn.co/image/ab6761610000101fa1b2c3d4e5f6a7b8c9d0e1f2",
+            },
+            {
+                name: "Kelvyn Boy",
+                image: "https://i.scdn.co/image/ab6761610000101f1234567890abcdef12345678",
+            },
+        ],
+    },
+    {
+        cover_image: "https://i.scdn.co/image/ab67616d0000b273b1c2d3e4f5a6b7c8d9e0f1a2",
+        title: "Apology",
+        release_date: new Date("2025-04-10"),
+        length: "3:55",
+        album: undefined,
+        artist: [
+            {
+                name: "Wendy Shay",
+                image: "https://i.scdn.co/image/ab6761610000101fb1c2d3e4f5a6b7c8d9e0f1a2",
+            },
+            {
+                name: "Mavado",
+                image: "https://i.scdn.co/image/ab6761610000101f234567890abcdef123456789",
+            },
+        ],
+    },
+    {
+        cover_image: "https://i.scdn.co/image/ab67616d0000b273c1d2e3f4a5b6c7d8e9f0a1b2",
+        title: "Check On Me",
+        release_date: new Date("2025-04-05"),
+        length: "3:40",
+        album: undefined,
+        artist: [
+            {
+                name: "Chop Daily",
+                image: "https://i.scdn.co/image/ab6761610000101fc1d2e3f4a5b6c7d8e9f0a1b2",
+            },
+            {
+                name: "Daboy Kenzi",
+                image: "https://i.scdn.co/image/ab6761610000101f34567890abcdef1234567890",
+            },
+        ],
+    },
+    {
+        cover_image: "https://i.scdn.co/image/ab67616d0000b273e1f2g3h4i5j6k7l8m9n0o1p2",
+        title: "Sacrifice",
+        release_date: new Date("2025-04-15"),
+        length: "3:50",
+        album: undefined,
+        artist: [
+            {
+                name: "Black Sherif",
+                image: "https://i.scdn.co/image/ab6761610000101fe1f2g3h4i5j6k7l8m9n0o1p2",
+            },
+        ],
+    },
+    {
+        cover_image: "https://i.scdn.co/image/ab67616d0000b273f1g2h3i4j5k6l7m8n9o0p1q2",
+        title: "Fire",
+        release_date: new Date("2025-04-12"),
+        length: "3:35",
+        album: undefined,
+        artist: [
+            {
+                name: "AratheJay",
+                image: "https://i.scdn.co/image/ab6761610000101ff1g2h3i4j5k6l7m8n9o0p1q2",
+            },
+            {
+                name: "Bella Shmurda",
+                image: "https://i.scdn.co/image/ab6761610000101f1234567890abcdef12345678",
+            },
+        ],
+    },
+    {
+        cover_image: "https://i.scdn.co/image/ab67616d0000b273g1h2i3j4k5l6m7n8o9p0q1r2",
+        title: "Foko!",
+        release_date: new Date("2025-03-25"),
+        length: "3:20",
+        album: undefined,
+        artist: [
+            {
+                name: "King Paluta",
+                image: "https://i.scdn.co/image/ab6761610000101fg1h2i3j4k5l6m7n8o9p0q1r2",
+            },
+        ],
+    },
+    {
+        cover_image: "https://i.scdn.co/image/ab67616d0000b273h1i2j3k4l5m6n7o8p9q0r1s2",
+        title: "Cheat On You",
+        release_date: new Date("2025-04-08"),
+        length: "3:25",
+        album: undefined,
+        artist: [
+            {
+                name: "KiDi",
+                image: "https://i.scdn.co/image/ab6761610000101fh1i2j3k4l5m6n7o8p9q0r1s2",
+            },
+            {
+                name: "Gyakie",
+                image: "https://i.scdn.co/image/ab6761610000101f1234567890abcdef12345678",
+            },
+        ],
+    },
+    {
+        cover_image: "https://i.scdn.co/image/ab67616d0000b273i1j2k3l4m5n6o7p8q9r0s1t2",
+        title: "Lomo Lomo",
+        release_date: new Date("2025-04-02"),
+        length: "3:15",
+        album: undefined,
+        artist: [
+            {
+                name: "KiDi",
+                image: "https://i.scdn.co/image/ab6761610000101fi1j2k3l4m5n6o7p8q9r0s1t2",
+            },
+            {
+                name: "Black Sherif",
+                image: "https://i.scdn.co/image/ab6761610000101f1234567890abcdef12345678",
+            },
+        ],
+    },
+    {
+        cover_image: "https://i.scdn.co/image/ab67616d0000b273j1k2l3m4n5o6p7q8r9s0t1u2",
+        title: "Vero",
+        release_date: new Date("2025-03-28"),
+        length: "3:10",
+        album: undefined,
+        artist: [
+            {
+                name: "Kelvyn Boy",
+                image: "https://i.scdn.co/image/ab6761610000101fj1k2l3m4n5o6p7q8r9s0t1u2",
+            },
+        ],
+    },
+    {
+        cover_image: "https://i.scdn.co/image/ab67616d0000b273k1l2m3n4o5p6q7r8s9t0u1v2",
+        title: "GoodSin",
+        release_date: new Date("2025-04-04"),
+        length: "3:05",
+        album: undefined,
+        artist: [
+            {
+                name: "Olivetheboy",
+                image: "https://i.scdn.co/image/ab6761610000101fk1l2m3n4o5p6q7r8s9t0u1v2",
+            },
+        ],
+    },
+    {
+        cover_image: "https://i.scdn.co/image/ab67616d0000b273l1m2n3o4p5q6r7s8t9u0v1w2",
+        title: "Asylum",
+        release_date: new Date("2025-04-06"),
+        length: "3:35",
+        album: undefined,
+        artist: [
+            {
+                name: "Olivetheboy",
+                image: "https://i.scdn.co/image/ab6761610000101fl1m2n3o4p5q6r7s8t9u0v1w2",
+            },
+        ],
+    },
+    {
+        cover_image: "https://i.scdn.co/image/ab67616d00001e02c9da7d7511880c6181753fb0",
+        title: "ITS OKAY",
+        release_date: new Date(),
+        length: "1:16",
+        album: undefined,
+        artist: [{
+            name: "Yaw Craft",
+            image: "https://i.scdn.co/image/ab6761610000101ff910ceb013e7f17069ffdb64",
+        }],
+    },
+    {
+        cover_image: "https://i.scdn.co/image/ab67616d00001e02a1fe10fa7f5c09554cb9c9a8",
+        title: "Bring me higher",
+        release_date: new Date(),
+        length: "3:55",
+        album: "CLEAR AS MUD",
+        artist: [{
+            name: "KDT",
+            image: "https://i.scdn.co/image/ab6761610000e5ebe4bc66d06cde64daf1897aa4",
+        },
+        {
+            name: "Yaw Craft",
+            image: "https://i.scdn.co/image/ab6761610000101ff910ceb013e7f17069ffdb64"
+        }],
+    },
+    {
+        cover_image: "https://i.scdn.co/image/ab67616d00001e028dc0d801766a5aa6a33cbe37",
+        title: "Intro",
+        release_date: new Date(),
+        length: "0:36",
+        album: "Honestly, Nevermind",
+        artist: [{
+            name: "Drake",
+            image: "https://i.scdn.co/image/ab6761610000101f4293385d324db8558179afd9",
+        }],
+    },
+    {
+        cover_image: "https://i.scdn.co/image/ab67616d00001e028dc0d801766a5aa6a33cbe37",
+        title: "Falling Back",
+        release_date: new Date(),
+        length: "4:26",
+        album: "Honestly, Nevermind",
+        artist: [{
+            name: "Drake",
+            image: "https://i.scdn.co/image/ab6761610000101f4293385d324db8558179afd9",
+        }],
+    },
+    {
+        cover_image: "https://i.scdn.co/image/ab67616d00001e028dc0d801766a5aa6a33cbe37",
+        title: "Texts Go Green",
+        release_date: new Date(),
+        length: "5:08",
+        album: "Honestly, Nevermind",
+        artist: [{
+            name: "Drake",
+            image: "https://i.scdn.co/image/ab6761610000101f4293385d324db8558179afd9",
+        }],
+    },
+    {
+        cover_image: "https://i.scdn.co/image/ab67616d00001e028dc0d801766a5aa6a33cbe37",
+        title: "Currents",
+        release_date: new Date(),
+        length: "2:37",
+        album: "Honestly, Nevermind",
+        artist: [{
+            name: "Drake",
+            image: "https://i.scdn.co/image/ab6761610000101f4293385d324db8558179afd9",
+        }],
+    },
+    {
+        cover_image: "https://i.scdn.co/image/ab67616d00001e028dc0d801766a5aa6a33cbe37",
+        title: "A Keeper",
+        release_date: new Date(),
+        length: "2:37",
+        album: "Honestly, Nevermind",
+        artist: [{
+            name: "Drake",
+            image: "https://i.scdn.co/image/ab6761610000101f4293385d324db8558179afd9",
+        }],
+    },
+    {
+        cover_image: "https://i.scdn.co/image/ab67616d00001e028dc0d801766a5aa6a33cbe37",
+        title: "Calling My Name",
+        release_date: new Date(),
+        length: "2:09",
+        album: "Honestly, Nevermind",
+        artist: [{
+            name: "Drake",
+            image: "https://i.scdn.co/image/ab6761610000101f4293385d324db8558179afd9",
+        }],
+    },
+    {
+        cover_image: "https://i.scdn.co/image/ab67616d00001e028dc0d801766a5aa6a33cbe37",
+        title: "Sticky",
+        release_date: new Date(),
+        length: "4:03",
+        album: "Honestly, Nevermind",
+        artist: [{
+            name: "Drake",
+            image: "https://i.scdn.co/image/ab6761610000101f4293385d324db8558179afd9",
+        }],
+    },
+    {
+        cover_image: "https://i.scdn.co/image/ab67616d00001e028dc0d801766a5aa6a33cbe37",
+        title: "Massive",
+        release_date: new Date(),
+        length: "5:36",
+        album: "Honestly, Nevermind",
+        artist: [{
+            name: "Drake",
+            image: "https://i.scdn.co/image/ab6761610000101f4293385d324db8558179afd9",
+        }],
+    },
+    {
+        cover_image: "https://i.scdn.co/image/ab67616d00001e028dc0d801766a5aa6a33cbe37",
+        title: "Fight's Booked",
+        release_date: new Date(),
+        length: "4:14",
+        album: "Honestly, Nevermind",
+        artist: [{
+            name: "Drake",
+            image: "https://i.scdn.co/image/ab6761610000101f4293385d324db8558179afd9",
+        }],
+    },
+    {
+        cover_image: "https://i.scdn.co/image/ab67616d00001e028dc0d801766a5aa6a33cbe37",
+        title: "Overdrive",
+        release_date: new Date(),
+        length: "3:22",
+        album: "Honestly, Nevermind",
+        artist: [{
+            name: "Drake",
+            image: "https://i.scdn.co/image/ab6761610000101f4293385d324db8558179afd9",
+        }],
+    },
+    {
+        cover_image: "https://i.scdn.co/image/ab67616d00001e028dc0d801766a5aa6a33cbe37",
+        title: "Down Hill",
+        release_date: new Date(),
+        length: "4:10",
+        album: "Honestly, Nevermind",
+        artist: [{
+            name: "Drake",
+            image: "https://i.scdn.co/image/ab6761610000101f4293385d324db8558179afd9",
+        }],
+    },
+    {
+        cover_image: "https://i.scdn.co/image/ab67616d00001e028dc0d801766a5aa6a33cbe37",
+        title: "Tie That Binds",
+        release_date: new Date(),
+        length: "5:36",
+        album: "Honestly, Nevermind",
+        artist: [
+            {
+                name: "Drake",
+                image: "https://i.scdn.co/image/ab6761610000101f4293385d324db8558179afd9",
+            }
+        ],
+    },
+    {
+        cover_image: "https://i.scdn.co/image/ab67616d00001e028dc0d801766a5aa6a33cbe37",
+        title: "Liability",
+        release_date: new Date(),
+        length: "3:57",
+        album: "Honestly, Nevermind",
+        artist: [
+            {
+                name: "Drake",
+                image: "https://i.scdn.co/image/ab6761610000101f4293385d324db8558179afd9",
+            }
+        ],
+    },
+    {
+        cover_image: "https://i.scdn.co/image/ab67616d00001e028dc0d801766a5aa6a33cbe37",
+        title: "jimmy Cooks (feat. 21 Savage)",
+        release_date: new Date(),
+        length: "3:57",
+        album: "Honestly, Nevermind",
+        artist: [
+            {
+                name: "Drake",
+                image: "https://i.scdn.co/image/ab6761610000101f4293385d324db8558179afd9",
+            },
+            {
+                name: "21 Savage",
+                image: "https://i.scdn.co/image/ab6761610000e5eb4f8f76117470957c0e81e5b2",
+            },
+        ],
+    },
+
+    {
+        cover_image: "https://i.scdn.co/image/ab67616d0000b273m1n2o3p4q5r6s7t8u9v0w1x2",
+        title: "Gyae Me",
+        release_date: new Date("2025-03-30"),
+        length: "3:20",
+        album: undefined,
+        artist: [
+            {
+                name: "DJ Boat",
+                image: "https://i.scdn.co/image/ab6761610000101fm1n2o3p4q5r6s7t8u9v0w1x2",
+            },
+            {
+                name: "Supa Gaeta",
+                image: "https://i.scdn.co/image/ab6761610000101f0987654321abcdef98765432",
+            },
+        ],
+    },
+    {
+        cover_image: "https://i.scdn.co/image/ab67616d0000b273n1o2p3q4r5s6t7u8v9w0x1y2",
+        title: "Energy",
+        release_date: new Date("2025-04-03"),
+        length: "3:30",
+        album: undefined,
+        artist: [
+            {
+                name: "R2Bees",
+                image: "https://i.scdn.co/image/ab6761610000101fn1o2p3q4r5s6t7u8v9w0x1y2",
+            },
+        ],
+    },
+    {
+        cover_image: "https://i.scdn.co/image/ab67616d0000b273o1p2q3r4s5t6u7v8w9x0y1z2",
+        title: "Problem No Dey Finish",
+        release_date: new Date("2025-04-11"),
+        length: "3:45",
+        album: undefined,
+        artist: [
+            {
+                name: "Fameye",
+                image: "https://i.scdn.co/image/ab6761610000101fo1p2q3r4s5t6u7v8w9x0y1z2",
+            },
+        ],
+    },
+    {
+        cover_image: "https://i.scdn.co/image/ab67616d0000b273p1q2r3s4t5u6v7w8x9y0z1a2",
+        title: "Pray",
+        release_date: new Date("2025-03-26"),
+        length: "4:05",
+        album: undefined,
+        artist: [
+            {
+                name: "D Jay",
+                image: "https://i.scdn.co/image/ab6761610000101fp1q2r3s4t5u6v7w8x9y0z1a2",
+            },
+        ],
+    },
+    {
+        cover_image: "https://i.scdn.co/image/ab67616d0000b273q1r2s3t4u5v6w7x8y9z0a1b2",
+        title: "Poppin",
+        release_date: new Date("2025-04-07"),
+        length: "3:25",
+        album: undefined,
+        artist: [
+            {
+                name: "Amaarae",
+                image: "https://i.scdn.co/image/ab6761610000101fq1r2s3t4u5v6w7x8y9z0a1b2",
+            },
+        ],
+    },
+    {
+        cover_image: "https://i.scdn.co/image/ab67616d0000b273r1s2t3u4v5w6x7y8z9a0b1c2",
+        title: "Kwaku The Traveller",
+        release_date: new Date("2022-03-31"),
+        length: "3:55",
+        album: undefined,
+        artist: [
+            {
+                name: "Black Sherif",
+                image: "https://i.scdn.co/image/ab6761610000101fr1s2t3u4v5w6x7y8z9a0b1c2",
+            },
+        ],
+    }
+
+
+]
